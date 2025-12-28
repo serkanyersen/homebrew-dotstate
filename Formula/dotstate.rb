d99252be20ffe506d5b11512f48df615c2b6f767
Serkan Yersen <serkanyersen@gmail.com>
2025-12-28 11:58:41 -0800

    fix sha

--- Formula/dotstate.rb
+++ Formula/dotstate.rb
@@ -6,8 +6,12 @@ class Dotstate < Formula
 
   # Determine the correct binary asset based on architecture
   # URLs and SHA256s are automatically updated by GitHub Actions on release
-  @19dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
-  @19dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
+  if Hardware::CPU.intel?
+    url "https://github.com/serkanyersen/dotstate/releases/download/v0.1.10/dotstate-x86_64-apple-darwin.tar.gz"
+    sha256 "5810c1dfd218b38c43fd7f3c2128c18990d736bc1cbd8bd06adb3983a5e62906"
+  else
+    url "https://github.com/serkanyersen/dotstate/releases/download/v0.1.10/dotstate-aarch64-apple-darwin.tar.gz"
+    sha256 "ae4da986880ea6b013800a601c626f361f4f0bb144fc8b139f6530f96d02e72f"
   end
 
   # No build dependencies needed - we're using pre-built binaries
